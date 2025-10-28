047ff22f6cc9a8e949450da3443543247ef15485
Thant Zin Soe <6631501147@lamduan.mfu.ac.th>
2025-10-29 01:13:51 +0700

    historyforusermodify

--- project2/lib/view/user/history_user.dart
+++ project2/lib/view/user/history_user.dart
@@ -9,111 +9,142 @@ class HistoryPage extends StatelessWidget {
     final List<Map<String, String>> bookingHistory = [
       {
         "name": "Ronaldo",
-        "room": "Room 100",
-        "date": "28 Oct 2025 8:00am - 10.00am",
-        "purpose": "Work with friend",
-        "lecturer": "Ajarn abc",
-        "status": "Pending",
-      },
-      {
-        "name": "Ronaldo",
+        "email": "Ronaldo123@gmail.com",
         "room": "Room 101",
-        "date": "28 Oct 2025 10:00am - 12.00pm",
-        "purpose": "Study",
-        "lecturer": "Ajarn xyz",
+        "time": "8:00pm - 10:00pm",
+        "reason": "Work with friend",
+        "approvedBy": "Ajarn ABC",
         "status": "Approved",
       },
       {
         "name": "Ronaldo",
-        "room": "Room 102",
-        "date": "28 Oct 2025 1:00pm - 3.00pm",
-        "purpose": "Group meeting",
-        "lecturer": "Ajarn abc",
+        "email": "Ronaldo123@gmail.com",
+        "room": "Room 103",
+        "time": "10:00pm - 12:00pm",
+        "reason": "Work with friend",
+        "approvedBy": "Ajarn ABC",
         "status": "Rejected",
+      },
+      {
+        "name": "Ronaldo",
+        "email": "Ronaldo123@gmail.com",
+        "room": "Room 102",
+        "time": "8:00pm - 10:00pm",
+        "reason": "Work with friend",
+        "approvedBy": "_",
+        "status": "Pending",
       },
     ];
 
     return Scaffold(
       backgroundColor: const Color(0xFFF5F5F5),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Booking History',
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-
-                    // Booking
-                    ...bookingHistory.map((item) {
-                      Color statusColor;
-                      if (item['status'] == 'Approved') {
-                        statusColor = Colors.green;
-                      } else if (item['status'] == 'Pending') {
-                        statusColor = Colors.orange;
-                      } else {
-                        statusColor = Colors.red;
-                      }
-
-                      return Container(
-                        margin: const EdgeInsets.only(bottom: 16),
-                        padding: const EdgeInsets.all(16),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade100,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            _infoRow(Icons.person, item['name']!),
-                            const SizedBox(height: 6),
-                            _infoRow(Icons.location_on, item['room']!),
-                            const SizedBox(height: 6),
-                            _infoRow(Icons.access_time, item['date']!),
-                            const SizedBox(height: 6),
-                            _infoRow(Icons.assignment, item['purpose']!),
-                            const SizedBox(height: 6),
-                            _infoRow(Icons.person_outline, item['lecturer']!),
-                            const SizedBox(height: 10),
-
-                            Align(
-                              alignment: Alignment.centerRight,
-                              child: Container(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal: 18,
-                                  vertical: 6,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: statusColor,
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                                child: Text(
-                                  item['status']!,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }),
-                  ],
-                ),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Booking History',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+
+              //  bookings
+              ...bookingHistory.map((item) {
+                Color statusColor;
+                IconData statusIcon;
+                String statusLabel;
+
+                if (item['status'] == 'Approved') {
+                  statusColor = const Color(0xFF4CAF50); // green
+                  statusIcon = Icons.check_circle_outline;
+                  statusLabel = 'Approved by: ${item['approvedBy']}';
+                } else if (item['status'] == 'Rejected') {
+                  statusColor = const Color(0xFFF44336); // red
+                  statusIcon = Icons.cancel_outlined;
+                  statusLabel = 'Rejected by: ${item['approvedBy']}';
+                } else {
+                  statusColor = const Color(0xFFE6D60A); // yellow
+
+                  statusIcon = Icons.remove_circle_outline;
+
+                  statusLabel = 'Approved by: _';
+                }
+
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 18),
+                  padding: const EdgeInsets.all(18),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFEAE7E6),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _infoRow(Icons.person, 'Name: ${item['name']}'),
+                      const SizedBox(height: 8),
+                      _infoRow(Icons.email, 'Email: ${item['email']}'),
+                      const SizedBox(height: 8),
+                      _infoRow(Icons.location_on, 'Room: ${item['room']}'),
+                      const SizedBox(height: 8),
+                      _infoRow(Icons.access_time, 'Time: ${item['time']}'),
+                      const SizedBox(height: 8),
+                      _infoRow(Icons.notes, 'Reason: ${item['reason']}'),
+                      const SizedBox(height: 8),
+
+                      // Approved or Rejected by row
+                      Row(
+                        children: [
+                          Icon(
+                            statusIcon,
+                            color: item['status'] == 'Approved'
+                                ? Colors.green
+                                : item['status'] == 'Rejected'
+                                ? Colors.red
+                                : Colors.brown,
+                            size: 20,
+                          ),
+                          const SizedBox(width: 8),
+                          Text(
+                            statusLabel,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(height: 14),
+
+                      // Status Button
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 6,
+                          ),
+                          decoration: BoxDecoration(
+                            color: statusColor,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Text(
+                            item['status']!,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            ],
+          ),
         ),
       ),
 
@@ -130,9 +161,7 @@ class HistoryPage extends StatelessWidget {
               _BottomNavItem(
                 icon: Icons.history,
                 label: 'History',
-                onTap: () {
-                  // here
-                },
+                onTap: () {},
               ),
               _BottomNavItem(
                 icon: Icons.edit_note,
@@ -152,13 +181,18 @@ class HistoryPage extends StatelessWidget {
     );
   }
 
-  //  row for icons and text
   Widget _infoRow(IconData icon, String text) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Icon(icon, color: Colors.brown, size: 20),
-        const SizedBox(width: 8),
-        Expanded(child: Text(text, style: const TextStyle(fontSize: 15))),
+        const SizedBox(width: 10),
+        Expanded(
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: 15, color: Colors.black87),
+          ),
+        ),
       ],
     );
   }
@@ -200,3 +234,4 @@ class _BottomNavItem extends StatelessWidget {
     );
   }
 }
+
