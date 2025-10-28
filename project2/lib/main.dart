5d81785c1d263f8e9777f49231020c7e5f540815
noongopal12 <panuphong2712@gmail.com>
2025-10-29 01:00:42 +0700

    again3

--- project2/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:project2/view/login.dart';
-import 'package:project2/view/register.dart';
-
-void main() {
-  runApp(MaterialApp(home: LoginPage()));
-}
