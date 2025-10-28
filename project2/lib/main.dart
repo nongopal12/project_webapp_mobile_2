9fd1742ef6d1e4f1336a9d789638855e29fc9d3a
nongopal12 <panuphong2712@gmail.com>
2025-10-28 22:08:31 +0700

    login and register system

--- project2/lib/main.dart
+++ project2/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course/week09/Drawer_demo.dart';
-import 'package:flutter_course/week09/drawer_widget.dart';
+import 'package:project2/view/login.dart';
+import 'package:project2/view/register.dart';
 
 void main() {
-  runApp(MaterialApp(home: DrawerDemo()));
+  runApp(MaterialApp(home: LoginPage()));
 }
