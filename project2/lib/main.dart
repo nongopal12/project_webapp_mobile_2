c747ae867db9462518fd7c2425617de1c18041c7
noongopal12 <panuphong2712@gmail.com>
2025-10-28 23:10:35 +0700

    again1

--- project2/lib/main.dart
+++ project2/lib/main.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:project2/view/staff/dashboard.dart';
-// import 'package:project2/view/staff/dashboard.dart';
+import 'package:project2/view/login.dart';
+import 'package:project2/view/register.dart';
 
 void main() {
-  runApp(const _App());
-}
-
-class _App extends StatelessWidget {
-  const _App({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Room Booking System',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color(0xFFF5F5F5),
-      ),
-      home: const Dashboard(), // หน้าแรกเป็น Dashboard
-    );
-  }
+  runApp(MaterialApp(home: LoginPage()));
 }
